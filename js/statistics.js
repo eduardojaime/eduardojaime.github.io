a675235eb12cf3b356f921dd5c2e62b5415883ef
Eduardo Jaime <eduardojaime86@gmail.com>
2023-04-07 08:32:46 -0400

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $.get("https://4qstatisticsapi.azurewebsites.net/api/statistics?site=eduardojaime.github.io").done(function(data){ console.log(data); });
+    $.get("https://4qstatisticsapiv2.azurewebsites.net/api/statistics?site=eduardojaime.github.io").done(function(data){ console.log(data); });
 });
